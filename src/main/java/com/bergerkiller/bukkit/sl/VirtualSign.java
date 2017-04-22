d04b0e486a11c32c3ff4fe931e00f70f09f78b0a
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2017-04-22 16:50:45 +0200

    Fix tabs and fix potential NPE when worlds are unloaded

--- src/main/java/com/bergerkiller/bukkit/sl/VirtualSign.java
+++ src/main/java/com/bergerkiller/bukkit/sl/VirtualSign.java
@@ -31,182 +31,209 @@ import com.bergerkiller.bukkit.sl.API.Variables;
  * It takes care of per-player text and text updating in general.
  */
 public class VirtualSign extends VirtualSignStore {
-	private final BlockLocation location;
-	private Sign sign;
-	private final String[] oldlines;
-	private final HashMap<String, VirtualLines> playerlines = new HashMap<String, VirtualLines>();
-	private final VirtualLines defaultlines;
-	private HashSet<VirtualLines> outofrange = new HashSet<VirtualLines>();
-	private final ToggledState loaded = new ToggledState(false);
-	private int signcheckcounter = 0;
+    private final BlockLocation location;
+    private Sign sign;
+    private final String[] oldlines;
+    private final HashMap<String, VirtualLines> playerlines = new HashMap<String, VirtualLines>();
+    private final VirtualLines defaultlines;
+    private HashSet<VirtualLines> outofrange = new HashSet<VirtualLines>();
+    private final ToggledState loaded = new ToggledState(false);
+    private int signcheckcounter = 0;
 
-	protected VirtualSign(BlockLocation location, String[] lines) {
-		this.location = location;
-		this.loaded.set(this.location.isLoaded());
-		if (lines == null || lines.length < VirtualLines.LINE_COUNT) {
-			if (this.loaded.get()) {
-				this.sign = BlockUtil.getSign(location.getBlock());
-				if (this.sign == null) {
-					this.loaded.clear();
-				}
-			}
-			if (this.sign == null) {
-				lines = new String[VirtualLines.LINE_COUNT];
-				Arrays.fill(lines, "");
-			} else {
-				lines = this.sign.getLines();
-			}
-		}
-		this.oldlines = LogicUtil.cloneArray(lines);
-		this.defaultlines = new VirtualLines(lines);
-	}
+    protected VirtualSign(BlockLocation location, String[] lines) {
+        this.location = location;
+        this.loaded.set(this.location.isLoaded());
+        if (lines == null || lines.length < VirtualLines.LINE_COUNT) {
+            if (this.loaded.get()) {
+                this.sign = BlockUtil.getSign(location.getBlock());
+                if (this.sign == null) {
+                    this.loaded.clear();
+                }
+            }
+            if (this.sign == null) {
+                lines = new String[VirtualLines.LINE_COUNT];
+                Arrays.fill(lines, "");
+            } else {
+                lines = this.sign.getLines();
+            }
+        }
+        this.oldlines = LogicUtil.cloneArray(lines);
+        this.defaultlines = new VirtualLines(lines);
+    }
 
-	public void remove() {
-		remove(this.getBlock());
-	}
+    public void remove() {
+        remove(this.getBlock());
+    }
 
-	public void resetLines() {
-		this.playerlines.clear();
-	}
+    public void resetLines() {
+        this.playerlines.clear();
+    }
 
-	public void resetLines(Player player) {
-		resetLines(player.getName());
-	}
+    public void resetLines(Player player) {
+        resetLines(player.getName());
+    }
 
-	public void resetLines(String playerName) {
-		playerlines.remove(playerName);
-	}
+    public void resetLines(String playerName) {
+        playerlines.remove(playerName);
+    }
 
-	public void invalidate(Player player) {
-		getLines(player).setChanged();
-	}
+    public void invalidate(Player player) {
+        getLines(player).setChanged();
+    }
 
-	public void invalidate(String player) {
-		getLines(player).setChanged();
-	}
+    public void invalidate(String player) {
+        getLines(player).setChanged();
+    }
 
-	public VirtualLines getLines(String playerName) {
-		if (playerName == null) {
-			return getLines();
-		}
-		VirtualLines lines = playerlines.get(playerName);
-		if (lines == null) {
-			lines = new VirtualLines(defaultlines.get());
-			lines.setChanged(true);
-			playerlines.put(playerName, lines);
-		}
-		return lines;
-	}
+    public VirtualLines getLines(String playerName) {
+        if (playerName == null) {
+            return getLines();
+        }
+        VirtualLines lines = playerlines.get(playerName);
+        if (lines == null) {
+            lines = new VirtualLines(defaultlines.get());
+            lines.setChanged(true);
+            playerlines.put(playerName, lines);
+        }
+        return lines;
+    }
 
-	public VirtualLines getLines(Player player) {
-		if (player == null) {
-			return getLines();
-		}
-		return getLines(player.getName());
-	}
+    public VirtualLines getLines(Player player) {
+        if (player == null) {
+            return getLines();
+        }
+        return getLines(player.getName());
+    }
 
-	public VirtualLines getLines() {
-		return this.defaultlines;
-	}
+    public VirtualLines getLines() {
+        return this.defaultlines;
+    }
 
-	public void setDefaultLine(int index, String value) {
-		getLines().set(index, value);
-	}
+    public void setDefaultLine(int index, String value) {
+        getLines().set(index, value);
+    }
 
-	public void setLine(int index, String value, String... players) {
-//		System.out.println("Set line "+index+" to "+value+" for "+(players==null?null:players.length));
-		if (players == null || players.length == 0) {
-			//Set all lines to this value at this index
-			for (VirtualLines lines : playerlines.values()) {
-				lines.set(index, value);
-			}
-			getLines().set(index, value);
-		} else {
-			for (String player : players) {
-			    VirtualLines lines = getLines(player);
-			    lines.set(index, value);
-				this.sendLines(lines, Bukkit.getPlayer(player));
-			}
-		}
-	}
+    public void setLine(int index, String value, String... players) {
+//        System.out.println("Set line "+index+" to "+value+" for "+(players==null?null:players.length));
+        if (players == null || players.length == 0) {
+            //Set all lines to this value at this index
+            for (VirtualLines lines : playerlines.values()) {
+                lines.set(index, value);
+            }
+            getLines().set(index, value);
+        } else {
+            for (String player : players) {
+                VirtualLines lines = getLines(player);
+                lines.set(index, value);
+                this.sendLines(lines, Bukkit.getPlayer(player));
+            }
+        }
+    }
 
-	public void restoreRealLine(int line) {
-	    setLine(line, getRealLine(line));
-	}
+    public void restoreRealLine(int line) {
+        setLine(line, getRealLine(line));
+    }
 
-	public String getLine(int index) {
-		return getLine(index, null);
-	}
+    public String getLine(int index) {
+        return getLine(index, null);
+    }
 
-	public String getLine(int index, String player) {
-		return getLines(player).get(index);
-	}
+    public String getLine(int index, String player) {
+        return getLines(player).get(index);
+    }
 
-	public String[] getRealLines() {
-		if (this.sign == null) {
-			return this.oldlines;
-		} else {
-			return this.sign.getLines();
-		}
-	}
+    public String[] getRealLines() {
+        if (this.sign == null) {
+            return this.oldlines;
+        } else {
+            return this.sign.getLines();
+        }
+    }
 
-	public String getRealLine(int index) {
-		if (this.sign == null) {
-			return this.oldlines[index];
-		} else {
-			return this.sign.getLine(index);
-		}
-	}
+    public String getRealLine(int index) {
+        if (this.sign == null) {
+            return this.oldlines[index];
+        } else {
+            return this.sign.getLine(index);
+        }
+    }
 
-	public void setRealLine(int index, String line) {
-		this.sign.setLine(index, line);
-	}
+    public void setRealLine(int index, String line) {
+        this.sign.setLine(index, line);
+    }
 
-	public World getWorld() {
-		return this.location.getWorld();
-	}
+    /**
+     * Gets the world this sign is in. If the world is unloaded,
+     * this function returns null.
+     * 
+     * @return world the sign is in
+     */
+    public World getWorld() {
+        return this.location.getWorld();
+    }
 
-	public int getX() {
-		return this.location.x;
-	}
+    public int getX() {
+        return this.location.x;
+    }
 
-	public int getY() {
-		return this.location.y;
-	}
+    public int getY() {
+        return this.location.y;
+    }
 
-	public int getZ() {
-		return this.location.z;
-	}
+    public int getZ() {
+        return this.location.z;
+    }
 
-	public int getChunkX() {
-		return this.getX() >> 4;
-	}
+    public int getChunkX() {
+        return this.getX() >> 4;
+    }
 
-	public int getChunkZ() {
-		return this.getZ() >> 4;
-	}
+    public int getChunkZ() {
+        return this.getZ() >> 4;
+    }
 
-	public Block getBlock() {
-		return this.location.getBlock();
-	}
+    /**
+     * Gets the block this sign is at. If the world the isgn is in is unloaded,
+     * this function will return null.
+     * 
+     * @return block of the sign
+     */
+    public Block getBlock() {
+        return this.location.getBlock();
+    }
 
-	public Location getLocation() {
-		return this.location.getLocation();
-	}
+    /**
+     * Gets the location of the sign. If the world the sign is in is unloaded,
+     * this function will return null.
+     * 
+     * @return location the sign is in
+     */
+    public Location getLocation() {
+        return this.location.getLocation();
+    }
 
-	public boolean isLoaded() {
-		return this.loaded.get();
-	}
+    /**
+     * Gets the position of the sign, consisting of block coordinates and world name.
+     * 
+     * @return block location
+     */
+    public BlockLocation getPosition() {
+        return this.location;
+    }
 
-	/**
-	 * Updates the sign contents to the update packet belonging to this sign
-	 * 
-	 * @param viewer of this sign
-	 * @param updatePacket for this sign
-	 */
-	public void applyToPacket(Player viewer, CommonPacket updatePacket) {
+    public boolean isLoaded() {
+        return this.loaded.get();
+    }
+
+    /**
+     * Updates the sign contents to the update packet belonging to this sign
+     * 
+     * @param viewer of this sign
+     * @param updatePacket for this sign
+     */
+    public void applyToPacket(Player viewer, CommonPacket updatePacket) {
         applyToPacket(getLines(viewer), updatePacket);
-	}
+    }
 
     /**
      * Updates the sign contents to the update packet belonging to this sign
@@ -214,7 +241,7 @@ public class VirtualSign extends VirtualSignStore {
      * @param virtual line information to apply
      * @param updatePacket for this sign
      */
-	public void applyToPacket(VirtualLines lines, CommonPacket updatePacket) {
+    public void applyToPacket(VirtualLines lines, CommonPacket updatePacket) {
         if (updatePacket.getType() != PacketType.OUT_TILE_ENTITY_DATA) return;
 
         CommonTagCompound compound = updatePacket.read(PacketType.OUT_TILE_ENTITY_DATA.data);
@@ -232,126 +259,126 @@ public class VirtualSign extends VirtualSignStore {
 
         // ====================================================================
         updatePacket.write(PacketType.OUT_TILE_ENTITY_DATA.data, compound);
-	}
+    }
 
-	/**
-	 * Performs a Sign validation check to see whether this Virtual Sign is still valid (a Sign block).
-	 * If the validation fails, this Sign is removed and False is returned.
-	 * 
-	 * @return True if the sign is valid, False if not
-	 */
-	public boolean validate() {
-		if (!this.loaded.get()) {
-			// Unloaded: until loaded we consider it to be valid
-			return true;
-		}
-		final Block block = this.getBlock();
-		if (!MaterialUtil.ISSIGN.get(block)) {
-			this.remove();
-			return false;
-		}
-		if (this.sign == null) {
-			this.sign = BlockUtil.getSign(this.getBlock());
-		}
-		if (this.sign == null || this.sign.getLines() == null) {
-			this.remove();
-			return false;
-		}
-		return true;
-	}
+    /**
+     * Performs a Sign validation check to see whether this Virtual Sign is still valid (a Sign block).
+     * If the validation fails, this Sign is removed and False is returned.
+     * 
+     * @return True if the sign is valid, False if not
+     */
+    public boolean validate() {
+        if (!this.loaded.get()) {
+            // Unloaded: until loaded we consider it to be valid
+            return true;
+        }
+        final Block block = this.getBlock();
+        if (!MaterialUtil.ISSIGN.get(block)) {
+            this.remove();
+            return false;
+        }
+        if (this.sign == null) {
+            this.sign = BlockUtil.getSign(this.getBlock());
+        }
+        if (this.sign == null || this.sign.getLines() == null) {
+            this.remove();
+            return false;
+        }
+        return true;
+    }
 
-	public boolean isInRange(Player player) {
-		if (!PlayerUtil.isChunkVisible(player, this.getChunkX(), this.getChunkZ())) {
-			return false;
-		}
-		if (player.getWorld() != this.getWorld()) {
-			return false;
-		}
-		return EntityUtil.isNearBlock(player, this.getX(), this.getZ(), 60);
-	}
+    public boolean isInRange(Player player) {
+        if (!PlayerUtil.isChunkVisible(player, this.getChunkX(), this.getChunkZ())) {
+            return false;
+        }
+        if (player.getWorld() != this.getWorld()) {
+            return false;
+        }
+        return EntityUtil.isNearBlock(player, this.getX(), this.getZ(), 60);
+    }
 
-	@Override
-	public String toString() {
-		return "VirtualSign {" + this.getX() + ", " + this.getY() + ", " + this.getZ() + ", " + this.getWorld().getName() + "}";
-	}
+    @Override
+    public String toString() {
+        return "VirtualSign {" + this.getX() + ", " + this.getY() + ", " + this.getZ() + ", " + this.getWorld().getName() + "}";
+    }
 
-	/**
-	 * Loads or unloads this Sign
-	 * 
-	 * @param loaded state to set to
-	 */
-	public void setLoaded(boolean loaded) {
-		if (loaded) {
-			if (this.loaded.set()) {
-				this.sign = null;
-				this.validate();
-			}
-		} else if (this.loaded.clear()) {
-			this.sign = null;
-		}
-	}
+    /**
+     * Loads or unloads this Sign
+     * 
+     * @param loaded state to set to
+     */
+    public void setLoaded(boolean loaded) {
+        if (loaded) {
+            if (this.loaded.set()) {
+                this.sign = null;
+                this.validate();
+            }
+        } else if (this.loaded.clear()) {
+            this.sign = null;
+        }
+    }
 
-	/**
-	 * Updates all nearby players with the live text information
-	 */
-	public void update() {
-		// Check whether the area this sign is at, is loaded
-		this.setLoaded(this.location.isLoaded());
-		if (!this.isLoaded()) {
-			return;
-		}
+    /**
+     * Updates all nearby players with the live text information
+     */
+    public void update() {
+        // Check whether the area this sign is at, is loaded
+        this.setLoaded(this.location.isLoaded());
+        if (!this.isLoaded()) {
+            return;
+        }
 
-		// Refresh the Sign state now and then (just in case the tile got swapped or destroyed)
-		if (signcheckcounter++ % 20 == 0) {
-			this.sign = null;
-		}
+        // Refresh the Sign state now and then (just in case the tile got swapped or destroyed)
+        if (signcheckcounter++ % 20 == 0) {
+            this.sign = null;
+        }
 
-		// Sanity check: is this sign still there?
-		if (!this.validate()) {
-			return;
-		}
+        // Sanity check: is this sign still there?
+        if (!this.validate()) {
+            return;
+        }
 
-		// Real-time changes to the text (external cause)
-		for (int i = 0; i < 4; i++) {
-			if (!this.oldlines[i].equals(this.sign.getLine(i))) {
-				Block signblock = this.getBlock();
-				String varname = Variables.parseVariableName(this.oldlines[i]);
-				if (varname != null) {
-					Variables.get(varname).removeLocation(signblock, i);
-				}
-				this.oldlines[i] = this.sign.getLine(i);
-				this.setLine(i, this.oldlines[i]);
-				varname = Variables.parseVariableName(this.oldlines[i]);
-				if (varname != null) {
-					Variables.get(varname).addLocation(signblock, i);
-				}
-			}
-		}
+        // Real-time changes to the text (external cause)
+        for (int i = 0; i < 4; i++) {
+            if (!this.oldlines[i].equals(this.sign.getLine(i))) {
+                Block signblock = this.getBlock();
+                String varname = Variables.parseVariableName(this.oldlines[i]);
+                if (varname != null) {
+                    Variables.get(varname).removeLocation(signblock, i);
+                }
+                this.oldlines[i] = this.sign.getLine(i);
+                this.setLine(i, this.oldlines[i]);
+                varname = Variables.parseVariableName(this.oldlines[i]);
+                if (varname != null) {
+                    Variables.get(varname).addLocation(signblock, i);
+                }
+            }
+        }
 
-		// Send updated sign text to nearby players
-		for (Player player : WorldUtil.getPlayers(getWorld())) {
-			VirtualLines lines = getLines(player);
-			if (isInRange(player)) {
-				if (outofrange.remove(lines) || lines.hasChanged()) {
-					this.sendLines(lines, player);
-				}
-			} else {
-				outofrange.add(lines);
-			}
-		}
+        // Send updated sign text to nearby players
+        for (Player player : WorldUtil.getPlayers(getWorld())) {
+            VirtualLines lines = getLines(player);
+            if (isInRange(player)) {
+                if (outofrange.remove(lines) || lines.hasChanged()) {
+                    this.sendLines(lines, player);
+                }
+            } else {
+                outofrange.add(lines);
+            }
+        }
 
-		// All signs updated - they are no longer 'dirty'
-		this.defaultlines.setChanged(false);
-		for (VirtualLines lines : playerlines.values()) {
-			lines.setChanged(false);
-		}
-	}
+        // All signs updated - they are no longer 'dirty'
+        this.defaultlines.setChanged(false);
+        for (VirtualLines lines : playerlines.values()) {
+            lines.setChanged(false);
+        }
+    }
 
-	public void sendCurrentLines(Player player) {
-		sendLines(getLines(player), player);
-	}
+    public void sendCurrentLines(Player player) {
+        sendLines(getLines(player), player);
+    }
 
-	public void sendLines(VirtualLines lines, Player player) {
+    public void sendLines(VirtualLines lines, Player player) {
         if (SignLink.updateSigns && player != null) {
             CommonPacket updatePacket = BlockUtil.getUpdatePacket(sign);
             if (updatePacket != null) {
@@ -362,6 +389,6 @@ public class VirtualSign extends VirtualSignStore {
                 SLListener.ignore = false;
             }
         }
-	}
+    }
 
 }
--- src/main/java/com/bergerkiller/bukkit/sl/VirtualSignStore.java
+++ src/main/java/com/bergerkiller/bukkit/sl/VirtualSignStore.java
@@ -20,147 +20,157 @@ import com.bergerkiller.bukkit.sl.API.Variables;
  * This allows storing, obtaining and global management of Virtual Signs.
  */
 public class VirtualSignStore {
-	private static BlockMap<VirtualSign> virtualSigns;
+    private static BlockMap<VirtualSign> virtualSigns;
 
-	public static void deinit() {
-		virtualSigns.clear();
-		virtualSigns = null;
-	}
+    public static void deinit() {
+        virtualSigns.clear();
+        virtualSigns = null;
+    }
 
-	public static void init() {
-		virtualSigns = new BlockMap<VirtualSign>();
-	}
+    public static void init() {
+        virtualSigns = new BlockMap<VirtualSign>();
+    }
 
-	public static synchronized VirtualSign add(Block block, String[] lines) {
-		if (virtualSigns == null) {
-			return null;
-		}
-		BlockLocation loc = new BlockLocation(block);
-		VirtualSign vsign = new VirtualSign(loc, lines);
-		virtualSigns.put(loc, vsign);
-		return vsign;
-	}
+    public static synchronized VirtualSign add(Block block, String[] lines) {
+        if (virtualSigns == null) {
+            return null;
+        }
+        BlockLocation loc = new BlockLocation(block);
+        VirtualSign vsign = new VirtualSign(loc, lines);
+        virtualSigns.put(loc, vsign);
+        return vsign;
+    }
 
-	public static VirtualSign add(Block signBlock) {
-		return add(signBlock, null);
-	}
+    public static VirtualSign add(Block signBlock) {
+        return add(signBlock, null);
+    }
 
-	public static synchronized VirtualSign get(Location at) {
-		return get(at.getBlock());
-	}
+    public static synchronized VirtualSign get(Location at) {
+        return get(at.getBlock());
+    }
 
-	public static synchronized VirtualSign get(World world, IntVector3 position) {
-	    return virtualSigns == null ? null : virtualSigns.get(world, position);
-	}
-	
-	public static synchronized VirtualSign get(Block b) {
-		return virtualSigns == null ? null : virtualSigns.get(b);
-	}
+    public static synchronized VirtualSign get(World world, IntVector3 position) {
+        return virtualSigns == null ? null : virtualSigns.get(world, position);
+    }
+    
+    public static synchronized VirtualSign get(Block b) {
+        return virtualSigns == null ? null : virtualSigns.get(b);
+    }
 
-	public static synchronized VirtualSign getOrCreate(Location at) {
-		return getOrCreate(at.getBlock());
-	}
+    public static synchronized VirtualSign getOrCreate(Location at) {
+        return getOrCreate(at.getBlock());
+    }
 
-	/**
-	 * Gets or creates a Virtual Sign for a Sign Block specified.
-	 * If the block specified is not a sign at all, null is returned.
-	 * 
-	 * @param b block of the Sign
-	 * @return the Virtual Sign at this Block
-	 */
-	public static synchronized VirtualSign getOrCreate(Block b) {
-		if (virtualSigns == null) {
-			return null;
-		}
-		if (MaterialUtil.ISSIGN.get(b)) {
-			VirtualSign sign = virtualSigns.get(b);
-			if (sign == null || !sign.validate()) {
-				sign = add(b);
-			}
-			return sign;
-		} else {
-			virtualSigns.remove(b);
-			return null;
-		}
-	}
+    /**
+     * Gets or creates a Virtual Sign for a Sign Block specified.
+     * If the block specified is not a sign at all, null is returned.
+     * 
+     * @param b block of the Sign
+     * @return the Virtual Sign at this Block
+     */
+    public static synchronized VirtualSign getOrCreate(Block b) {
+        if (virtualSigns == null) {
+            return null;
+        }
+        if (MaterialUtil.ISSIGN.get(b)) {
+            VirtualSign sign = virtualSigns.get(b);
+            if (sign == null || !sign.validate()) {
+                sign = add(b);
+            }
+            return sign;
+        } else {
+            virtualSigns.remove(b);
+            return null;
+        }
+    }
 
-	public static synchronized VirtualSign[] getAll() {
-		if (virtualSigns == null) {
-			return null;
-		}
-		return virtualSigns.values().toArray(new VirtualSign[0]);
-	}
+    public static synchronized VirtualSign[] getAll() {
+        if (virtualSigns == null) {
+            return null;
+        }
+        return virtualSigns.values().toArray(new VirtualSign[0]);
+    }
 
-	public static synchronized boolean exists(Location at) {
-		return exists(at.getBlock());
-	}
+    public static synchronized boolean exists(Location at) {
+        return exists(at.getBlock());
+    }
 
-	public static synchronized boolean exists(Block at) {
-		return virtualSigns != null && virtualSigns.containsKey(at);
-	}
+    public static synchronized boolean exists(Block at) {
+        return virtualSigns != null && virtualSigns.containsKey(at);
+    }
 
-	/**
-	 * Removes a Virtual Sign from the storage.
-	 * 
-	 * @param signBlock to remove
-	 * @return True if a Virtual Sign was removed, False if not
-	 */
-	public static synchronized boolean remove(Block signBlock) {
-		if (virtualSigns == null || virtualSigns.remove(signBlock) == null) {
-			return false;
-		}
-		for (Variable var : Variables.getAll()) {
-			var.removeLocation(signBlock);
-		}	
-		return true;
-	}
+    /**
+     * Removes a Virtual Sign from the storage.
+     * 
+     * @param signBlock to remove
+     * @return True if a Virtual Sign was removed, False if not
+     */
+    public static synchronized boolean remove(Block signBlock) {
+        if (virtualSigns == null || virtualSigns.remove(signBlock) == null) {
+            return false;
+        }
+        for (Variable var : Variables.getAll()) {
+            var.removeLocation(signBlock);
+        }
+        return true;
+    }
 
-	public static synchronized void removeAll(World world) {
-		Iterator<VirtualSign> iter = virtualSigns.values().iterator();
-		while (iter.hasNext()) {
-			if (iter.next().getWorld() == world) {
-				iter.remove();
-			}
-		}
-	}
+    public static synchronized void removeAll(World world) {
+        Iterator<VirtualSign> iter = virtualSigns.values().iterator();
+        while (iter.hasNext()) {
+            if (iter.next().getWorld() == world) {
+                iter.remove();
+            }
+        }
+    }
 
-	public static void updateAll() {
-		for (VirtualSign sign : getAll()) {
-			sign.update();
-		}
-	}
+    public static void updateAll() {
+        for (VirtualSign sign : getAll()) {
+            sign.update();
+        }
+    }
 
-	public static void forcedUpdate(final Player forplayer, long delay) {
-		if (forplayer == null) {
-			return;
-		}
-		new Task(SignLink.plugin) {
-			public void run() {
-				forcedUpdate(forplayer);
-			}
-		}.start(delay);
-	}
+    public static void forcedUpdate(final Player forplayer, long delay) {
+        if (forplayer == null) {
+            return;
+        }
+        new Task(SignLink.plugin) {
+            public void run() {
+                forcedUpdate(forplayer);
+            }
+        }.start(delay);
+    }
 
-	public static synchronized void forcedUpdate(Player forplayer) {
-		if (forplayer == null) {
-			return;
-		}
-		for (VirtualSign sign : virtualSigns.values()) {
-			if(sign.getWorld().equals(forplayer.getWorld())&&sign.getLocation().distance(forplayer.getLocation())<25)sign.sendCurrentLines(forplayer);
-		}
-	}
+    public static synchronized void forcedUpdate(Player forplayer) {
+        if (forplayer == null || forplayer.getWorld() == null) {
+            return;
+        }
+        final int SIGN_RADIUS = 25;
+        BlockLocation playerpos = new BlockLocation(forplayer.getLocation());
+        for (VirtualSign sign : virtualSigns.values()) {
+            BlockLocation pos = sign.getPosition();
+            if (pos.world.equals(playerpos.world)) {
+                int dx = playerpos.x - pos.x;
+                int dy = playerpos.y - pos.y;
+                int dz = playerpos.z - pos.z;
+                if (((dx * dx) + (dy * dy) + (dz * dz)) < (SIGN_RADIUS * SIGN_RADIUS)) {
+                    sign.sendCurrentLines(forplayer);
+                }
+            }
+        }
+    }
 
-	public static synchronized void clearPlayer(String playerName) {
-		for (VirtualSign sign : virtualSigns.values()) {
-			sign.resetLines(playerName);
-		}
-	}
+    public static synchronized void clearPlayer(String playerName) {
+        for (VirtualSign sign : virtualSigns.values()) {
+            sign.resetLines(playerName);
+        }
+    }
 
-	public static synchronized void invalidateAll(Player player) {
-		for (VirtualSign vs : virtualSigns.values()) {
-			if (vs.isInRange(player)) {
-				vs.invalidate(player);
-			}
-		}
-	}
+    public static synchronized void invalidateAll(Player player) {
+        for (VirtualSign vs : virtualSigns.values()) {
+            if (vs.isInRange(player)) {
+                vs.invalidate(player);
+            }
+        }
+    }
 }
