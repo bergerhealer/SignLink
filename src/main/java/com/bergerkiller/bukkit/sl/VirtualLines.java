373ca99acf2cf689a690c362e618adad85effe13
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2023-06-10 21:10:51 +0200

    Working towards 1.20-v1: Initial 1.20 support for front sign text
    Signed-off-by: Irmo van den Berge <irmo.vandenberge@ziggo.nl>

--- src/main/java/com/bergerkiller/bukkit/sl/VirtualLines.java
+++ src/main/java/com/bergerkiller/bukkit/sl/VirtualLines.java
@@ -1,11 +1,13 @@
 package com.bergerkiller.bukkit.sl;
 
+import com.bergerkiller.bukkit.common.internal.CommonCapabilities;
 import com.bergerkiller.bukkit.common.nbt.CommonTagCompound;
+import com.bergerkiller.bukkit.common.nbt.CommonTagList;
 import com.bergerkiller.bukkit.common.wrappers.ChatText;
 
 public class VirtualLines {
     //public static final int MAX_LINE_LENGTH = 15; /* Legacy no longer applies */
-    private static final String[] SIGN_META_LINE_KEYS = new String[] { "Text1", "Text2", "Text3", "Text4" };
+    private static final String[] SIGN_META_LINE_KEYS_PRE_1_20 = new String[] { "Text1", "Text2", "Text3", "Text4" };
     public static final int LINE_WIDTH_LIMIT = 90;
     public static final int LINE_COUNT = 4;
     private String[] lines = new String[LINE_COUNT];
@@ -21,19 +23,57 @@ public class VirtualLines {
     }
 
     public boolean isDifferentThanMetadata(CommonTagCompound metadata) {
-        for (int i = 0; i < VirtualLines.LINE_COUNT; i++) {
-            String text = ChatText.fromJson(metadata.getValue(SIGN_META_LINE_KEYS[i], "")).getMessage();
-            if (!get(i).equals(text)) {
-                return true;
+        if (CommonCapabilities.HAS_SIGN_BACK_TEXT) {
+            // >= 1.20
+            CommonTagCompound front_text = metadata.get("front_text", CommonTagCompound.class);
+            if (front_text != null) {
+                CommonTagList messages = front_text.get("messages", CommonTagList.class);
+                if (messages.size() == VirtualLines.LINE_COUNT) {
+                    for (int i = 0; i < VirtualLines.LINE_COUNT; i++) {
+                        String text = ChatText.fromJson(messages.getValue(i, "")).getMessage();
+                        if (!get(i).equals(text)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        } else {
+            // <= 1.19.4
+            for (int i = 0; i < VirtualLines.LINE_COUNT; i++) {
+                String text = ChatText.fromJson(metadata.getValue(SIGN_META_LINE_KEYS_PRE_1_20[i], "")).getMessage();
+                if (!get(i).equals(text)) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
     public void applyToSignMetadata(CommonTagCompound metadata) {
-        for (int i = 0; i < VirtualLines.LINE_COUNT; i++) {
-            String text = ChatText.fromMessage(get(i)).getJson();
-            metadata.putValue(SIGN_META_LINE_KEYS[i], text);
+        if (CommonCapabilities.HAS_SIGN_BACK_TEXT) {
+            // >= 1.20
+            CommonTagCompound front_text = metadata.createCompound("front_text");
+            CommonTagList messages = front_text.createList("messages");
+            if (messages.size() == VirtualLines.LINE_COUNT) {
+                // Set lines
+                for (int i = 0; i < VirtualLines.LINE_COUNT; i++) {
+                    String text = ChatText.fromMessage(get(i)).getJson();
+                    messages.setValue(i, text);
+                }
+            } else {
+                // Clear and re-add
+                messages.clear();
+                for (int i = 0; i < VirtualLines.LINE_COUNT; i++) {
+                    String text = ChatText.fromMessage(get(i)).getJson();
+                    messages.addValue(text);
+                }
+            }
+        } else {
+            // <= 1.19.4
+            for (int i = 0; i < VirtualLines.LINE_COUNT; i++) {
+                String text = ChatText.fromMessage(get(i)).getJson();
+                metadata.putValue(SIGN_META_LINE_KEYS_PRE_1_20[i], text);
+            }
         }
     }
 
